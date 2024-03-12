e33f3bafd790a44863ad185a52bd6dff3807c9a2
김상훈 <k1016h@naver.com>
2024-03-12 22:18:07 +0900

    Fix : catch errors

--- frontend/src/electron/TitleBar.tsx
+++ frontend/src/electron/TitleBar.tsx
@@ -64,13 +64,13 @@ export default function TitleBar() {
   return (
     <div className="fixed flex justify-end z-10 border" style={{ WebkitAppRegion: "drag", width: "100%", borderColor: "rgba(0, 0, 0, 0)", backgroundColor: "#262626" } as React.CSSProperties }>
       <div style={{ WebkitAppRegion: "no-drag" } as React.CSSProperties}>
-        <Button onClick={minimize} className="">
+        <Button onClick={minimize} className="" size="icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/></svg>  
         </Button>&nbsp;
-        <Button onClick={maximize} className="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
+        <Button onClick={maximize} className="" size="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
         </Button>&nbsp;
-        <Button onClick={hidden} className="">
+        <Button onClick={hidden} className="" size="icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
         </Button>
       </div>
