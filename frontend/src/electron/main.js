b88bb9df4aa42cc37c3e79a46166d52ddc1dc049
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-12 17:45:16 +0900

    fix: init front

--- frontend/src/electron/main.js
+++ frontend/src/electron/main.js
@@ -1,75 +1,75 @@
 import axios from "axios";
 import https from "https";
 import dotenv from "dotenv";
-import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 import electronLocalshortcut from "electron-localshortcut";
 import { app, ipcMain, BrowserWindow, globalShortcut, Tray, Menu, nativeImage } from "electron";
 
-dotenv.config();
-const BASE_URL = 'http://localhost:3000';
-
 let mainWindow;
-let tray;
 function createWindow() {
   // 브라우저 창을 생성합니다.
   mainWindow = new BrowserWindow({
-    width: 1700,
+    width: 1600,
     height: 900,
     frame: false,
 
-    icon: join(dirname(fileURLToPath(import.meta.url)), "icon.png"),
-    webPreferences: {preload: join(dirname(fileURLToPath(import.meta.url)), "preload.js")},
+    icon: join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png"),
+    webPreferences: { preload: join(dirname(fileURLToPath(import.meta.url)), "preload.js") },
   });
 
-  mainWindow.loadURL(BASE_URL);
-
-  electronLocalshortcut.register(mainWindow, "F12", () => {console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools()});
-
-  ipcMain.on("minimize", (event) => {mainWindow.minimize()});
-
+  // 타이틀 바 옵션
+  ipcMain.on("hidden", (event) => { mainWindow.hide() });
+  ipcMain.on("minimize", (event) => { mainWindow.minimize() });
   ipcMain.on("maximize", (event) => {
     if (mainWindow.isMaximized()) {
-        mainWindow.restore();
-        console.log("restoring")}
-    else {
-      mainWindow.maximize(); console.log("maximizing")
-    }});
+      mainWindow.restore();
+      console.log("restoring");
+    } else {
+      mainWindow.maximize();
+      console.log("maximizing");
+    }
+  });
 
-  ipcMain.on("hidden", (event) => {mainWindow.hide()});
+  // 일렉트론 연결 URL
+  dotenv.config();
+  const BASE_URL = 'http://localhost:3000';
+  mainWindow.loadURL(BASE_URL);
 
-  app.on("window-all-closed", () => {if (process.platform !== "darwin") {app.quit()}});
-
-  app.on("activate", () => {if (BrowserWindow.getAllWindows().length === 0) {createWindow()}});
+  // F12 개발자 도구
+  electronLocalshortcut.register(mainWindow, "F12", () => { console.log("F12 is pressed"); mainWindow.webContents.toggleDevTools() });
 }
 
 // 이 메소드는 Electron의 초기화가 완료되고
 // 브라우저 윈도우가 생성될 준비가 되었을때 호출된다.
 app.whenReady().then(createWindow).then(async () => {
-    const iconPath = join(dirname(fileURLToPath(import.meta.url)), "icon.png");
-    const icon = nativeImage.createFromPath(iconPath);
 
-    globalShortcut.register('F5', () => {
-      let win = BrowserWindow.getFocusedWindow();
-      if (win) {
-        win.reload();
-        connect();
-      }
-    });
+  // 기본 생성 세팅
+  app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit() } });
+  app.on("activate", () => { if (BrowserWindow.getAllWindows().length === 0) { createWindow() } });
 
-    tray = new Tray(icon);
+  // 트레이 세팅
+  const icon = nativeImage.createFromPath(join(dirname(fileURLToPath(import.meta.url)), "../../public/icon.png"));
+  const tray = new Tray(icon);
 
-    const contextMenu = Menu.buildFromTemplate([
-      { label: "켜기", type: "normal", click: () => mainWindow.show() },
-      { label: "끄기", type: "normal", click: () => app.quit() },
-    ]);
+  // 트레이 메뉴 옵션
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "켜기", type: "normal", click: () => mainWindow.show() },
+    { label: "끄기", type: "normal", click: () => app.quit() },
+  ]);
 
-    tray.setToolTip("정보처리기사 대비반");
-    tray.setContextMenu(contextMenu);
-    tray.on("double-click", () => {mainWindow.show()});
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip("정보처리기사 대비반");
+  tray.on("double-click", () => { mainWindow.show() });
 
-    app.on('will-quit', () => {
-      // 애플리케이션이 종료되기 전에 모든 단축키를 해제합니다.
-      globalShortcut.unregisterAll();
-    });
+  // F5 키를 누르면 현재 포커스된 창 새로고침
+  globalShortcut.register('F5', () => {
+    let win = BrowserWindow.getFocusedWindow();
+    if (win) { win.reload() }
   });
+
+  app.on('will-quit', () => {
+    // 애플리케이션이 종료되기 전에 단축키 해제
+    globalShortcut.unregisterAll();
+  });
+});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,13 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import "@/globals.css";
-import ReactDOM from "react-dom/client";
-import App from "@/routes/App";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-////////////////일렉트론 컴포넌트/////////////////////
-const electron = window.electron;
-import TitleBar from "@/electron/TitleBar.tsx";
+//////////////// electron components ////////////////
+import TitleBar from "@/electron/TitleBar";
 /////////////////////////////////////////////////////
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      {typeof electron !== "undefined" && <TitleBar />}
-      <RouterProvider router={router} />
-  </ThemeProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <>
+    {typeof window.electron !== "undefined" && <TitleBar />}
+    <App />
+  </>,
+)
