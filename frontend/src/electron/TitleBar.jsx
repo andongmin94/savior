22496bc8c0ac80163d6beac1bb0619206195b125
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-13 20:22:30 +0900

    feat: tsx to jsx

--- frontend/src/electron/TitleBar.jsx
+++ frontend/src/electron/TitleBar.jsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva } from "class-variance-authority";
+import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -32,24 +32,15 @@ const buttonVariants = cva(
   }
 );
 
-interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
-
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
+  const Comp = asChild ? Slot : "button"
+  return (
+    (<Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props} />)
+  );
+})
 
 export default function TitleBar() {
   const minimize = () => {
@@ -62,8 +53,8 @@ export default function TitleBar() {
     window.electron.send("hidden");
   };
   return (
-    <div className="fixed flex justify-end z-10 border" style={{ WebkitAppRegion: "drag", width: "100%", borderColor: "rgba(0, 0, 0, 0)", backgroundColor: "#262626" } as React.CSSProperties }>
-      <div style={{ WebkitAppRegion: "no-drag" } as React.CSSProperties}>
+    <div className="fixed flex justify-end z-10 border" style={{ WebkitAppRegion: "drag", width: "100%", borderColor: "rgba(0, 0, 0, 0)", backgroundColor: "#262626" }}>
+      <div style={{ WebkitAppRegion: "no-drag" }}>
         <Button onClick={minimize} className="" size="icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/></svg>  
         </Button>&nbsp;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import "@/globals.css";
 //////////////// electron components ////////////////
 import TitleBar from "@/electron/TitleBar";
 /////////////////////////////////////////////////////
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     {typeof window.electron !== "undefined" && <TitleBar />}
     <App />
