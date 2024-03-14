b0cce1af0fe0322ffc544c9f07263749d85f0b46
장준영 <wnsdud2889@naver.com>
2024-03-14 17:34:08 +0900

    Feat : dependencies & packages
    Details:
    - Author: 장준영 <wnsdud2889@naver.com>
    - Commit date: 2024-03-14
    - Files changed: 6
    - Line changes: +53 -0
    - Key files:
      - backend/springboot/build.gradle
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/controller/UserController.java
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/dto/UserDto.java
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/entity/User.java
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/repository/UserRepository.java

---
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/entity/User.java
@@ -0,0 +1,14 @@
+package kr.ac.baekgoo.springboot.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    private Long userId;
+}
---
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/service/UserService.java
@@ -0,0 +1,7 @@
+package kr.ac.baekgoo.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
