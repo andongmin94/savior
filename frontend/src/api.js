3590d33a75179a8452b634e8d6479c72cc19312d
안동민 <andongmin94@gmail.com>
2024-03-19 10:05:41 +0900

    Feat : add api
    Details:
    - Author: 안동민 <andongmin94@gmail.com>
    - Commit date: 2024-03-19
    - Files changed: 1
    - Line changes: +30 -0
    - Key files:
      - frontend/src/api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const Axios = axios.create({
+  baseURL: 'http://j10d109.p.ssafy.io:8080',
+  // baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTION',
+    'Access-Control-Allow-Headers': 'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization',
+    'Access-Control-Expose-Headers': 'Content-Length,Content-Range',
+  },
+});
+
+function getAxios() {
+  const token = localStorage.getItem('token');
+  Axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  return Axios;
+}
+
+const AxiosDjango = axios.create({
+  baseURL: 'http://j10d109.p.ssafy.io:8000',
+  // baseURL: 'http://localhost:8000',
+});
+
+function getAxiosDjango() {
+  return AxiosDjango;
+}
+
+export { getAxios, getAxiosDjango };
