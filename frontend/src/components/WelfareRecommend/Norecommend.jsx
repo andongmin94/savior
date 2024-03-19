31f50475e7b4f2c45e287ceaccca99f9a638feb4
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-19 17:26:38 +0900

    refactor: remove unused styled-components import

--- frontend/src/components/WelfareRecommend/Norecommend.jsx
+++ frontend/src/components/WelfareRecommend/Norecommend.jsx
@@ -1,8 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
-import styled from "styled-components";
-
 export default function Norecommend(props) {
   const navigate = useNavigate();
   return (
