1984197e42b4e01220185ff6cda44c6a74a4b992
안동민 <andongmin94@gmail.com>
2024-03-19 11:32:45 +0900

    Feat : styled to tailwind in welfare-recommend

--- frontend/src/pages/WelfareRecommend.jsx
+++ frontend/src/pages/WelfareRecommend.jsx
@@ -8,8 +8,6 @@ import { getAxios } from '@/api';
 import Norecommend from '@/components/WelfareRecommend/Norecommend';
 import LoginModal from '@/components/LoginModal';
 
-import styled from 'styled-components';
-
 const isLogin = () => {
   if (localStorage.getItem('token')) {
     return true;
@@ -48,52 +46,24 @@ function WelfareRecommend() {
     cards.length === 0 ? (
       <Norecommend profile={profile} name={name}></Norecommend>
     ) : (
-      <StyledContainer>
-        <StyledTop>
+      <div className="grid justify-center mx-auto" style={{ marginTop: '10vh', marginBottom: '5vh', gridColumn: '70vw' }}>
+        <div className="grid justify-center items-center" style={{ gridTemplateColumns: '20% 30% 50%', marginTop: '5vh' }}>
           <ProfileCard profile={profile} name={name} />
           <Chart />
           <LineChart />
-        </StyledTop>
-        <StyledMain>
+        </div>
+        <div className="gap-y-2vw w-70vw" style={{ gridRowGap: '2vh', width: '70vw' }}>
           <div>
             <h2 style={{ margin: '5% 1% -2% 0', fontWeight: '600' }}>{name}님에게 추천하는 복지</h2>
             <FilterSlide name={name} cards={cards} />
           </div>
           <RecommendSlid />
-        </StyledMain>
-      </StyledContainer>
+        </div>
+      </div>
     )
   ) : (
     <LoginModal />
   );
 }
-
-const StyledTop = styled.div`
-  display: grid;
-  grid-template-columns: 20% 30% 50%;
-  justify-content: center;
-  align-items: center;
-  margin-top: 5vh;
-  // margin-bottom: 5vh;
-`;
-const StyledMain = styled.div`
-  display: grid;
-  justify-content: center;
-  // margin-bottom: 5vh;
-  // margin-top: 5vh;
-  grid-row-gap: 2vh;
-  width: 70vw;
-`;
-const StyledContainer = styled.div`
-  display: grid;
-  justify-content: center;
-  margin-top: 10vh;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: auto;
-  grid-template-columns: 70vw;
-  font-family: 'Pretendard';
-  margin-bottom: 5vh;
-`;
 
 export default WelfareRecommend;
