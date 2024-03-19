5502f1dbac9cd905bae253d38858ebdcb0c66d38
안동민 <andongmin94@gmail.com>
2024-03-19 10:01:46 +0900

    Feat : modify main.jsx theme
    Details:
    - Author: 안동민 <andongmin94@gmail.com>
    - Commit date: 2024-03-19
    - Files changed: 2
    - Line changes: +83 -56
    - Key files:
      - frontend/src/globals.css
      - frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,40 @@
+import 'regenerator-runtime/runtime';
+import React from "react";
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import "@/globals.css";
+import App from "./App";
+import reportWebVitals from "@/reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import rootReducer from "@/reducers/index";
+import { applyMiddleware, createStore, compose } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import ScrollToTop from "@/pages/ScrollTop";
 //////////////// electron components ////////////////
 import TitleBar from "@/electron/TitleBar";
 /////////////////////////////////////////////////////
 
+const enhancer =
+  process.env.NODE_ENV === "production"
+    ? compose(applyMiddleware())
+    : composeWithDevTools(applyMiddleware(logger));
+
+const store = createStore(rootReducer, undefined, enhancer);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    {typeof window.electron !== "undefined" && <TitleBar />}
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Provider store={store}>
+      {typeof window.electron !== "undefined" && <TitleBar />}
+        <App />
+      </Provider>
+    </BrowserRouter>
   </>,
 )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
