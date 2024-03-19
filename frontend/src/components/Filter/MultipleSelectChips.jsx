d14b512641d8b196bf432588f25f21fd804aec21
안동민 <andongmin94@gmail.com>
2024-03-19 14:59:10 +0900

    Feat : optimize unuse module
    Details:
    - Author: 안동민 <andongmin94@gmail.com>
    - Commit date: 2024-03-19
    - Files changed: 19
    - Line changes: +5 -25
    - Key files:
      - frontend/src/App.jsx
      - frontend/src/components/Filter/MultipleSelectChips.jsx
      - frontend/src/components/Qna/Blank.jsx
      - frontend/src/components/Qna/BlankWrite.jsx
      - frontend/src/components/Search/PaginationBtn.jsx

--- frontend/src/components/Filter/MultipleSelectChips.jsx
+++ frontend/src/components/Filter/MultipleSelectChips.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { Chip, Typography, FormHelperText } from '@mui/material';
 
--- frontend/src/components/layout/layout.jsx
+++ frontend/src/components/layout/layout.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
