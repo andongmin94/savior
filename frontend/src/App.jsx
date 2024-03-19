c8a8f7987f7158ec5903115d102b12a6d9763320
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-19 10:05:49 +0900

    feat: modify app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import WelfareDetail from "./pages/WelfareDetail";
+import Main from "@/pages/Main";
+import Qna from "@/pages/Qna";
+import QnaCreate from "@/pages/QnaCreate";
+import QnaPatch from "@/pages/QnaPatch";
+import QnaDetail from "@/pages/QnaDetail";
+import WelfareSearch from "@/pages/WelfareSearch";
+import WelfareRecommend from "@/pages/WelfareRecommend";
+import CustomFilter from "@/pages/CustomFilter";
+import Profile from "@/pages/Profile";
+import NotFound from "@/pages/NotFound";
+import Layout from "@/components/layout/layout";
+import Manual from "@/pages/Manual";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="app">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/welfare/:welfareId" element={<WelfareDetail />} />
+          <Route path="/Qna" element={<Qna />} />
+          <Route path="/search" element={<WelfareSearch />} />
+          <Route path="/QnaCreate/" element={<QnaCreate />} />
+          <Route path="/QnaPatch/:qnaId" element={<QnaPatch />} />
+          <Route path="/QnaDetail/:qnaId" element={<QnaDetail />} />
+          <Route path="/filter" element={<CustomFilter />} />
+          <Route path="/recommend" element={<WelfareRecommend />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/manual" element={<Manual />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
