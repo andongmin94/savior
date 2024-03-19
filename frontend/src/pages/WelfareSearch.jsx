3d82f66325845ea2f181d1b6d93a0908ce1bb85b
안동민 <andongmin94@gmail.com>
2024-03-19 10:51:27 +0900

    Feat : delete styled in welfare-search

--- frontend/src/pages/WelfareSearch.jsx
+++ frontend/src/pages/WelfareSearch.jsx
@@ -6,28 +6,15 @@ import Keyword from '@/components/Search/Keyword';
 
 function Search(){
     return(
-        <StyledContainer>
+        <div className="grid justify-center" style={{ paddingTop: '30px', marginTop: '12vh' }}>
             <h2>통합검색</h2>
             <SearchBar/>
-            <StyledMain>
+            <div className="flex justify-between pt-20" style={{ paddingTop: '5vh' }}>
                 <ResultBoard />
                 <Keyword />
-            </StyledMain>
-        </StyledContainer>
+            </div>
+        </div>
     )
 };
-
-const StyledContainer = styled.div`
-    display: grid;
-    justify-content: center;
-    padding-top: 30px;
-    font-family: 'Pretendard';
-    margin-top: 12vh;
-`;
-const StyledMain = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding-top: 5vh;
-`;
 
 export default Search;
