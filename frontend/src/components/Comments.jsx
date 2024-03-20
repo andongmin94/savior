b870e2a6eb4bfc662a0ff4b685b5fb9e791414e0
안동민 <andongmin94@gmail.com>
2024-03-20 15:51:39 +0900

    Fix : linting comments

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,17 +1,16 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Button, Modal } from 'react-bootstrap';
-import { Comment } from '@mui/icons-material';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Button, Modal } from "react-bootstrap";
 
-import { getAxios } from '@/api';
+import { getAxios } from "@/api";
 
 export default function Comments(props) {
   const axios = getAxios();
-  const qnaId  = useParams().qnaId;
-  const [comment, setComment] = useState('');
-  const { id, content, getComment, name, getDate, checkDate} = props;
+  const qnaId = useParams().qnaId;
+  const [comment, setComment] = useState("");
+  const { id, content, getComment, name, getDate, checkDate } = props;
   const [editable, setEditable] = useState(false);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   const [writedate, setWritedate] = useState([]);
 
@@ -22,46 +21,35 @@ export default function Comments(props) {
   const handleShow = () => setShow(true);
 
   const deleteComment = async (Id) => {
-    await axios.delete(`/api/comment/${Id}`, {
-
-    })
+    await axios.delete(`/api/comment/${Id}`, {});
     getComment();
-
-  } 
+  };
 
   const updateComment = async (Id) => {
-    await axios.patch(`/api/comment/${Id}?content=${comment}`)
+    await axios.patch(`/api/comment/${Id}?content=${comment}`);
     getComment();
-
-  }
-  useEffect(()=> {
-
+  };
+  useEffect(() => {
     setComment(content);
     setUsername(name);
     // setUpdatedate(getDate);
     setWritedate(getDate);
-
-  }, [])
-
+  }, []);
 
   return editable === false ? (
     <div>
-      <p className='text-md'>
-        {comment}
-      </p>
-      { checkDate ? (
-
-          <div className="grid justify-between float-right">
-            작성자 : {username} / 등록일 : {getDate[0]}년 {getDate[1]}월 {getDate[2]}일 {getDate[3]}시 {getDate[4]}분
-          </div>
-        ):(
-          <div className="grid justify-between float-right">
-            작성자 : {username} / 수정일 : {getDate[0]}년 {getDate[1]}월 {getDate[2]}일 {getDate[3]}시 {getDate[4]}분
-          </div>
-      )
-
-      }
-
+      <p className="text-md">{comment}</p>
+      {checkDate ? (
+        <div className="grid justify-between float-right">
+          작성자 : {username} / 등록일 : {getDate[0]}년 {getDate[1]}월{" "}
+          {getDate[2]}일 {getDate[3]}시 {getDate[4]}분
+        </div>
+      ) : (
+        <div className="grid justify-between float-right">
+          작성자 : {username} / 수정일 : {getDate[0]}년 {getDate[1]}월{" "}
+          {getDate[2]}일 {getDate[3]}시 {getDate[4]}분
+        </div>
+      )}
       <Button
         variant="secondary"
         size="sm"
@@ -70,17 +58,17 @@ export default function Comments(props) {
         }}
       >
         수정
-      </Button>
-      {' '}
-      
+      </Button>{" "}
       <Button
         variant="danger"
-        size="sm" 
-        onClick={()=>{
+        size="sm"
+        onClick={() => {
           handleShow();
-        // deleteComment(props.id);
-      }}>삭제</Button>
-
+          // deleteComment(props.id);
+        }}
+      >
+        삭제
+      </Button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>댓글 삭제</Modal.Title>
@@ -105,22 +93,31 @@ export default function Comments(props) {
       <hr />
     </div>
   ) : (
-    <div className='flex mb-[5%]'>
-      <input type="text" value={comment} onChange={(e)=> (setComment(e.target.value))}></input>
-      <Button 
-        variant="secondary"
-        size="sm"
-        onClick={(e)=> {
-        updateComment(props.id);
-        setEditable(!editable)
-      }}>저장</Button>
-      {' '}
+    <div className="flex mb-[5%]">
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      ></input>
       <Button
         variant="secondary"
         size="sm"
-        onClick={(e)=> {
-          setEditable(!editable)
-        }}>취소</Button>
+        onClick={(e) => {
+          updateComment(props.id);
+          setEditable(!editable);
+        }}
+      >
+        저장
+      </Button>{" "}
+      <Button
+        variant="secondary"
+        size="sm"
+        onClick={(e) => {
+          setEditable(!editable);
+        }}
+      >
+        취소
+      </Button>
     </div>
   );
-};
+}
