1dfc1e82f507475e9f7833377ea7b815329fa75f
안동민 <andongmin94@gmail.com>
2024-03-20 14:31:33 +0900

    Feat : style to tailwind in child

--- frontend/src/components/Filter/Child.jsx
+++ frontend/src/components/Filter/Child.jsx
@@ -10,9 +10,7 @@ export default function ChildSelectBox({ child, setChild }) {
       id="selectChild"
       value={child}
       onChange={handleChange}
-      style={{
-        width: '250px',
-      }}
+      className='w-[250px]'
     >
       <option value="1">있음(출산예정/ 입양예정)</option>
       <option value="2">없음</option>
