0082102000ed540f3be45f012da4adf5c2ab0009
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-20 15:27:51 +0900

    feat: style to tailwind in welfare-recommend

--- frontend/src/pages/WelfareRecommend.jsx
+++ frontend/src/pages/WelfareRecommend.jsx
@@ -49,15 +49,15 @@ export default function WelfareRecommend() {
     cards.length === 0 ? (
       <Norecommend profile={profile} name={name}></Norecommend>
     ) : (
-      <div className="grid justify-center mx-auto" style={{ marginTop: '10vh', marginBottom: '5vh', gridColumn: '70vw' }}>
-        <div className="grid justify-center items-center" style={{ gridTemplateColumns: '20% 30% 50%', marginTop: '5vh' }}>
+      <div className="grid justify-center mx-auto mt-[10vh] mb-[5vh] grid-cols-[70vw]">
+        <div className="grid justify-center items-center mt-[5vh] grid-cols-[20%_30%_50%]">
           <ProfileCard profile={profile} name={name} />
           <Chart />
           <LineChart />
         </div>
-        <div className="gap-y-2vw w-70vw" style={{ gridRowGap: '2vh', width: '70vw' }}>
+        <div className="gap-y-[2vh] w-[70vw]">
           <div>
-            <h2 style={{ margin: '5% 1% -2% 0', fontWeight: '600' }}>{name}님에게 추천하는 복지</h2>
+            <h2 className="mt-[5%] mr-[1%] mb-[-2%] ml-0 font-semibold">{name}님에게 추천하는 복지</h2>
             <FilterSlide name={name} cards={cards} />
           </div>
           <RecommendSlid />
