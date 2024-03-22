036e93b7b06f8a6dcdd19aeeafb292be923b3d51
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-22 14:39:20 +0900

    style: format TitleBar component

--- frontend/src/electron/TitleBar.jsx
+++ frontend/src/electron/TitleBar.jsx
@@ -55,19 +55,19 @@ export default function TitleBar() {
   return (
     <>
     {typeof window.electron !== "undefined" && 
-    <div className="fixed flex justify-end w-full bg-[#262626] z-10" style={{ WebkitAppRegion: "drag" }}>
-      <div style={{ WebkitAppRegion: "no-drag" }}>
-        <Button onClick={minimize} size="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"/></svg>  
-        </Button>&nbsp;
-        <Button onClick={maximize} size="icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
-        </Button>&nbsp;
-        <Button onClick={hidden} size="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
-        </Button>
-      </div>
-    </div>}
+      <div className="fixed flex justify-end w-full bg-[#262626] z-10" style={{ WebkitAppRegion: "drag" }}>
+        <div style={{ WebkitAppRegion: "no-drag" }}>
+          <Button onClick={minimize} size="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"/></svg>  
+          </Button>&nbsp;
+          <Button onClick={maximize} size="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><rect width="18" height="18" x="3" y="3" rx="2"/></svg>
+          </Button>&nbsp;
+          <Button onClick={hidden} size="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+          </Button>
+        </div>
+      </div>}
     </>
   );
 }
