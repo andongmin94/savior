8b01ce6980c9422fb9c54eca9b0b6180fec2cf64
MalrangCow <malrangcow00@gmail.com>
2024-03-22 17:21:59 +0900

    Feat: set urls

--- backend/django/data_analyzer/urls.py
+++ backend/django/data_analyzer/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'data_analyzer'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('data/save', views.save_data),
+    path('data/dbscan', views.dbscan),
+    path('data/grouping', views.cosine_grouping),
 ]
