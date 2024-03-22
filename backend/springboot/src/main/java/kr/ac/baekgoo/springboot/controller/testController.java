5bccb4f967fb071a30e4fd5e2d9971e0b4eecabc
장준영 <wnsdud2889@naver.com>
2024-03-22 10:10:09 +0900

    Fix : fix testController, add dummy for resources

--- backend/springboot/src/main/java/kr/ac/baekgoo/springboot/controller/testController.java
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/controller/testController.java
@@ -1,9 +1,7 @@
 package kr.ac.baekgoo.springboot.controller;
 
 import kr.ac.baekgoo.springboot.common.ApiResponse;
-import kr.ac.baekgoo.springboot.entity.Keyword;
-import kr.ac.baekgoo.springboot.entity.User;
-import kr.ac.baekgoo.springboot.entity.Welfare;
+import kr.ac.baekgoo.springboot.domain.welfare.Welfare;
 import kr.ac.baekgoo.springboot.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +14,7 @@ import java.util.*;
 @RequiredArgsConstructor
 public class testController {
 
-    private final UserService userService;
     private final WelfareService welfareService;
-    private final KeywordService keywordService;
 
     @GetMapping("/{welfare_id}")
     public ApiResponse getwelfare(@PathVariable("welfare_id") Long welfare_id) {
