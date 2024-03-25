2a5a90b57f52a473872fb5cc2392a46b08c0fa0a
장준영 <wnsdud2889@naver.com>
2024-03-25 14:43:09 +0900

    Feat : Logging added
    Details:
    - Author: 장준영 <wnsdud2889@naver.com>
    - Commit date: 2024-03-25
    - Files changed: 6
    - Line changes: +83 -13
    - Key files:
      - backend/springboot/.gitignore
      - backend/springboot/build.gradle
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/config/security/SecurityConfig.java
      - backend/springboot/src/main/resources/application.yml
      - backend/springboot/src/main/resources/dummy

--- backend/springboot/src/main/java/kr/ac/baekgoo/springboot/config/security/SecurityConfig.java
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/config/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import kr.ac.baekgoo.springboot.oauth.service.CustomOAuth2UserService;
 import kr.ac.baekgoo.springboot.oauth.token.AuthTokenProvider;
 import kr.ac.baekgoo.springboot.repository.user.UserRefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -29,6 +30,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Log4j2
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -52,21 +54,27 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(AbstractHttpConfigurer::disable);
-        http.sessionManagement((sessionManagement)->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.formLogin(AbstractHttpConfigurer::disable);
-        http.httpBasic(AbstractHttpConfigurer::disable);
-        http.exceptionHandling((exceptionHandling)->exceptionHandling.authenticationEntryPoint(new RestAuthenticationEntryPoint()).accessDeniedHandler(tokenAccessDeniedHandler));
-        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(CorsUtils::isPreFlightRequest).permitAll().requestMatchers("/swagger-ui/**", "/v3/**", "/api/**", "/swagger-resources/**").permitAll().anyRequest().authenticated());
-        http.oauth2Login(oauth2 -> oauth2.authorizationEndpoint(authorization -> authorization.baseUri("/oauth2/authorization").authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
-                .redirectionEndpoint(redirection -> redirection.baseUri("/*/oauth2/code/*"))
+        System.out.println("securityFilterChain");
+        log.info("securityFilterChain start");
+        try {
+            http.cors(AbstractHttpConfigurer::disable);
+            http.sessionManagement((sessionManagement)->sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            http.csrf(AbstractHttpConfigurer::disable);
+            http.formLogin(AbstractHttpConfigurer::disable);
+            http.httpBasic(AbstractHttpConfigurer::disable);
+            http.exceptionHandling((exceptionHandling)->exceptionHandling.authenticationEntryPoint(new RestAuthenticationEntryPoint()).accessDeniedHandler(tokenAccessDeniedHandler));
+            http.authorizeHttpRequests(authorize -> authorize.requestMatchers(CorsUtils::isPreFlightRequest).permitAll().requestMatchers("/swagger-ui/**", "/v3/**", "/api/**", "/swagger-resources/**").permitAll().anyRequest().authenticated());
+            http.oauth2Login(oauth2 -> oauth2.authorizationEndpoint(authorization -> authorization.baseUri("/oauth2/authorization").authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository()))
+                    .redirectionEndpoint(redirection -> redirection.baseUri("/*/oauth2/code/*"))
                     .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
                     .successHandler(oAuth2AuthenticationSuccessHandler())
                     .failureHandler(oAuth2AuthenticationFailureHandler()));
 
-        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
+            http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+            log.info("securityFilterChain succeess");
+        } catch (Exception e) {
+            log.error("error : securityFilterChain", e);
+        }
         return http.getOrBuild();
     }
 
