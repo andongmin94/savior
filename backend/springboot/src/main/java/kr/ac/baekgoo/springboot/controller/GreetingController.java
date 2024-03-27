fe8fa99c727b40afb0254744db6fc371bd0e8529
오은아 <88129325+eunalove@users.noreply.github.com>
2024-03-27 22:36:12 +0900

    Test: backed test 추가

--- backend/springboot/src/main/java/kr/ac/baekgoo/springboot/controller/GreetingController.java
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/controller/GreetingController.java
@@ -1,14 +1,13 @@
-package kr.ac.baekgoo.springboot;
+package kr.ac.baekgoo.springboot.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin("*")
 @RestController
 public class GreetingController {
-
-    @GetMapping("/api/greeting")
+    @GetMapping("/greeting")
     public String greeting() {
         return "Hello from Spring Boot!";
     }
