8c119949dc4e6a979a74199a29251ed777d1a79d
안동민 <andongmin94@gmail.com>
2024-03-28 15:55:17 +0900

    Fix : delete response test in main
    Details:
    - Author: 안동민 <andongmin94@gmail.com>
    - Commit date: 2024-03-28
    - Files changed: 1
    - Line changes: +0 -14
    - Key files:
      - frontend/src/pages/Main.jsx

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -112,20 +112,6 @@ export default function Main() {
                   카카오톡으로 시작하기
                 </Button>
               ) : null}
-              &nbsp;
-              <Button className="mt-[10px] bg-[#ea580c] border-[#ea580c]"
-              onClick={() => {
-                axios.get(`/api/greeting`, {})
-                .then(response => {
-                  console.log(response);
-                })
-                .catch(error => {
-                  console.error(error);
-                });
-              }}
-                >
-                  회신 테스트
-                </Button>
             </div>
             <img src="Main-icon1.png" alt="main-image" width="400px" />
           </div>
