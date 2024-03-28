f88d8ddc96676a5167bd4faca2965281002778fb
장준영 <wnsdud2889@naver.com>
2024-03-28 11:46:42 +0900

    Fix : rollingfile gz-> log, redirectUri Fix : delete @EnableWebMvc
    Details:
    - Author: 장준영 <wnsdud2889@naver.com>
    - Commit date: 2024-03-28
    - Files changed: 3
    - Line changes: +25 -33
    - Key files:
      - backend/springboot/src/main/java/kr/ac/baekgoo/springboot/SpringbootApplication.java
      - backend/springboot/src/main/resources/application.yml
      - backend/springboot/src/main/resources/log4j2.yml

--- backend/springboot/src/main/java/kr/ac/baekgoo/springboot/SpringbootApplication.java
+++ backend/springboot/src/main/java/kr/ac/baekgoo/springboot/SpringbootApplication.java
@@ -5,9 +5,7 @@ import kr.ac.baekgoo.springboot.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 @EnableConfigurationProperties({
         CorsProperties.class,
