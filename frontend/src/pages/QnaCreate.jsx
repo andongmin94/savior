10abddfb984e9bb82bf32652895333b3c2d6217d
김상훈 <k1016h@naver.com>
2024-03-31 00:05:21 +0900

    Fix : modify theme in qna-create

--- frontend/src/pages/QnaCreate.jsx
+++ frontend/src/pages/QnaCreate.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import { useParams, useNavigate, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { Button, Container } from 'react-bootstrap';
-import HtmlReactParser from 'html-react-parser';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Button, Container } from "react-bootstrap";
+import HtmlReactParser from "html-react-parser";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
-import { getAxios } from '@/api';
+import { getAxios } from "@/api";
 
 function isLogin() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     return true;
   } else {
@@ -20,19 +20,19 @@ function isLogin() {
 export default function QnaCreate(props) {
   let navigate = useNavigate();
   let state = useSelector((state) => state);
-  let [title, setTitle] = useState('');
-  let [content, setContent] = useState('');
+  let [title, setTitle] = useState("");
+  let [content, setContent] = useState("");
 
   const qnaId = useParams().qnaId;
   const axios = getAxios();
 
   const createQna = async () => {
-    if (title == '') {
-      alert('제목을 입력해주세요');
-    } else if (content == '') {
-      alert('내용을 입력해주세요');
-    } else if (title !== '' && content !== '') {
-      await axios.post('/api/qna/mine', {
+    if (title == "") {
+      alert("제목을 입력해주세요");
+    } else if (content == "") {
+      alert("내용을 입력해주세요");
+    } else if (title !== "" && content !== "") {
+      await axios.post("/api/qna/mine", {
         title: title,
         content: content,
       });
@@ -41,7 +41,7 @@ export default function QnaCreate(props) {
   };
   const checkLogin = () => {
     if (!isLogin()) {
-      alert('로그인해주세요');
+      alert("로그인해주세요");
       navigate(`/`);
     }
   };
@@ -52,18 +52,18 @@ export default function QnaCreate(props) {
     <Container>
       {isLogin() ? (
         <div className="mx-auto w-[70%] mt-[15%] pb-[15%]">
-          <div className='text-center mb-[3%]'>
+          <div className="text-center mb-[3%]">
             <strong>고객센터</strong>
-            <div className='text-center text-[16px] mt-[5px]'>
+            <div className="text-center text-[16px] mt-[5px]">
               궁금한 점이나 문의 사항을 남겨주세요.
             </div>
           </div>
-          <div className='w-full mb-[2%]'>
+          <div className="w-full mb-[2%]">
             <p>제목</p>
             <input
               type="text"
               maxLength="50"
-              className='w-full border-[1px] border-solid border-[#cccccc] p-[10px]'
+              className="w-full border-[1px] border-solid border-[#cccccc] p-[10px]"
               onChange={(e) => {
                 setTitle(e.target.value);
               }}
@@ -78,20 +78,13 @@ export default function QnaCreate(props) {
             }}
           />
 
-          <div className='py-10 text-center'>
+          <div className="py-10 text-center">
             <Link to="/Qna">
-              <Button
-                className='bg-gray-500 border-gray-500'
-                variant="danger"
-                size="lg">
-                취소
-              </Button>
-            </Link>{' '}
+              <Button className="bg-gray-500 border-none">취소</Button>
+            </Link>{" "}
             {/* <Link to = '/Qna'> */}
             <Button
-              className='bg-[#ea580c] border-[#ea580c]'
-              size="lg"
-              variant="danger"
+              className="bg-blue-700 border-none"
               onClick={(e) => {
                 createQna();
               }}
