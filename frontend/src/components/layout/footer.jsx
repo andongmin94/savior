8ebfa8e8dc9314c25f612b263570b6f5740d3c3c
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-03-31 00:03:09 +0900

    fix: theme in footer

--- frontend/src/components/layout/footer.jsx
+++ frontend/src/components/layout/footer.jsx
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bg-orange-400 p-4">
+    <footer className="bg-blue-700 p-4">
       <div className="flex justify-center">
         <div className="flex justify-between items-center">
-          <img src="savior_logo/team.png" className="w-60" alt="logo" />
+          <img src="/savior_logo/team.png" className="w-60" alt="logo" />
         </div>
       </div>
     </footer>
