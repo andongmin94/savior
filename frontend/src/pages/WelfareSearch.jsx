10a8a08e1dd18d69eb04e8c8218bc370627c184b
김상훈 <k1016h@naver.com>
2024-03-31 00:05:09 +0900

    Fix : modify theme in welfare-search

--- frontend/src/pages/WelfareSearch.jsx
+++ frontend/src/pages/WelfareSearch.jsx
@@ -5,7 +5,7 @@ import ResultBoard from '@/components/Search/ResultBoard';
 export default function Search(){
     return(
         <div className="grid justify-center pt-[30px] mt-[12vh]">
-            <h2>통합검색</h2>
+            <h2 className='font-bold'>통합검색</h2>
             <SearchBar/>
             <div className="flex justify-between pt-[5vh]">
                 <ResultBoard />
