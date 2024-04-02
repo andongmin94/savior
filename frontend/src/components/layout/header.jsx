0e8b9418003b4f9c7e75eef68ad844757be584b3
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-04-03 03:03:03 +0900

    fix: modify bug

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -31,7 +31,16 @@ export default function Header() {
             </Nav.Link>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             {typeof window.electron === "undefined" &&
-            <Nav.Link href="https://k-asap-savior.s3.amazonaws.com/welfare_app/Savior_Setup.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=REMOVED&X-Amz-Date=20240402T173517Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=REMOVED">
+            <Nav.Link href="	
+            오전 2:39
+            
+            
+            
+            
+            
+            
+            
+            https://k-asap-savior.s3.amazonaws.com/welfare_app/Savior_Setup.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=REMOVED&X-Amz-Date=20240402T173517Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=REMOVED">
               <div className="text-white font-bold">다운로드</div>
             </Nav.Link>}
           </Nav>
