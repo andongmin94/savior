479feed028caf31947126f2bb0775ae28770570b
김상훈 <k1016h@naver.com>
2024-04-02 22:53:20 +0900

    Fix : modify ppt and header download link

--- frontend/src/components/layout/header.jsx
+++ frontend/src/components/layout/header.jsx
@@ -29,10 +29,11 @@ export default function Header() {
             <Nav.Link href="/manual">
               <div className="text-white font-bold">세이비어 이용방법</div>
             </Nav.Link>
-            {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Nav.Link href="/qna">
-              <div className="text-white font-bold">Q&A</div>
-            </Nav.Link> */}
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            {typeof window.electron === "undefined" &&
+            <Nav.Link href="https://k-asap-savior.s3.amazonaws.com/welfare_app/Savior_Setup.exe?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAYS2NXJFTSTXYG2XU%2F20240402%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Date=20240402T173517Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=5841408f3f1941f1d5deafdb1579c68eed529f792f8e60ef607665afb10b63ff">
+              <div className="text-white font-bold">다운로드</div>
+            </Nav.Link>}
           </Nav>
           <Login />
         </Container>
